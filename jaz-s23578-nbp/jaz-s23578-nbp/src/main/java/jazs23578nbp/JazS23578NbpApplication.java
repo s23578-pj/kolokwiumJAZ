28471890b77ef02daed28c749237ce0b072c8327
s23578-pj <73029891+s23578-pj@users.noreply.github.com>
2022-09-04 14:18:54 +0200

    Pierwsze pomyślne uruchomienie aplikacji

---
+++ jaz-s23578-nbp/jaz-s23578-nbp/src/main/java/jazs23578nbp/JazS23578NbpApplication.java
@@ -0,0 +1,13 @@
+package jazs23578nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23578NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23578NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s23578-nbp/jaz-s23578-nbp/src/test/java/jazs23578nbp/JazS23578NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package jazs23578nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23578NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
