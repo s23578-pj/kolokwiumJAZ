4338304342ab85f639caaca1cd1438a2ab9c233c
s23578-pj <73029891+s23578-pj@users.noreply.github.com>
2022-09-04 15:39:24 +0200

    Poprawienie błędów. Poprawienie  RestControllera
    Poprawienie połączenia z bazą danych, ustawienie poprawnego linku. Dodanie GetMappingu w RestControllerze oraz poprawienie kodu w tym package.

--- jaz-s23578-nbp/jaz-s23578-nbp/src/main/java/jazs23578nbp/Repository/NbpRestController.java
+++ jaz-s23578-nbp/jaz-s23578-nbp/src/main/java/jazs23578nbp/Repository/NbpRestController.java
@@ -1,4 +1,40 @@
 package jazs23578nbp.Repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jazs23578nbp.Service.NbpService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NbpRestController {
+    final NbpService nbpService;
+
+    public NbpRestController(NbpService nbpService) {
+        this.nbpService = nbpService;
+    }
+
+    /**
+     * Endpoint zapisuje srednia cene zlota w podanym zakresie czasu uwzgledniajac podana walute
+     * @param waluta Waluta w formacie trzy literowym
+     * @param startDate Data pocztakowa
+     * @param endDate Data koncowa
+     * @return zwracany jest obiekt NbpEntity
+     */
+    @GetMapping("/zloto/{waluta}/{startDate}/{endDate}")
+    public ResponseEntity<String> zloto(@PathVariable String waluta, @PathVariable String startDate,
+                                        @PathVariable String endDate) throws JsonProcessingException {
+        return ResponseEntity.ok(nbpService.obliczZloto(waluta, startDate, endDate));
+    }
+
+    /**
+     * Endpoint pobiera z bazy obiekt o podanym id
+     * @param id id elementu w bazie
+     * @return zwracany jest obiekt NbpEntity
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getById(@PathVariable Long id){
+        return ResponseEntity.ok(nbpService.getID(id));
+    }
 }
